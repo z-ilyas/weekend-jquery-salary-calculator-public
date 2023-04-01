5330636303f88a425a25a4783960536e10206cc2
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-01 16:51:18 -0500

    input and submit is working

--- javascript.js
+++ javascript.js
@@ -1,5 +1,26 @@
 $(document).ready(onReady);
 
 function onReady() {
-    
+
+$('#submit-btn').on('click', submitInfo);   
+}
+
+function submitInfo(event){
+event.preventDefault();
+
+let firstNameInput = $('#firstName-Input').val();
+let lastNameInput = $('#lastName-Input').val();
+let idInput = $('#id-Input').val();
+let titleInput = $('#title-Input').val();
+let annualSalaryInput = $('#annualSalary-Input').val();
+$('#employee-Info').append(`
+<tr>
+    <td>${firstNameInput}</td>
+    <td>${lastNameInput}</td>
+    <td>${idInput}</td>
+    <td>${titleInput}</td>
+    <td>${annualSalaryInput}</td>
+    <td><button class="delete-btn">❌</button></td>
+</tr>
+`);
 }
