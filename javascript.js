fdd59bef04caf818942e16584fb51c4bd6f37a6a
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-01 19:02:39 -0500

    submit button does not let anything remain in the input

--- javascript.js
+++ javascript.js
@@ -2,25 +2,45 @@ $(document).ready(onReady);
 
 function onReady() {
 
-$('#submit-btn').on('click', submitInfo);   
+$('#submit-btn').on('click', submitInfo);  
+$('#employee-Info').on('click','.delete-btn', deleteRow)
 }
 
 function submitInfo(event){
 event.preventDefault();
 
-let firstNameInput = $('#firstName-Input').val();
-let lastNameInput = $('#lastName-Input').val();
-let idInput = $('#id-Input').val();
-let titleInput = $('#title-Input').val();
-let annualSalaryInput = $('#annualSalary-Input').val();
+let firstNameInput = $('#firstName-Input')
+let lastNameInput = $('#lastName-Input')
+let idInput = $('#id-Input')
+let titleInput = $('#title-Input')
+let annualSalaryInput = $('#annualSalary-Input')
+
+firstNameInput.val('')
+lastNameInput.val('')
+idInput.val('')
+titleInput.val('')
+annualSalaryInput.val('')
+
+let firstName = firstNameInput.val()
+let lastName = lastNameInput.val()
+let id = idInput.val()
+let title = titleInput.val()
+let annualSalary = annualSalaryInput.val()
+
+
+
 $('#employee-Info').append(`
 <tr>
-    <td>${firstNameInput}</td>
-    <td>${lastNameInput}</td>
-    <td>${idInput}</td>
-    <td>${titleInput}</td>
-    <td>${annualSalaryInput}</td>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${id}</td>
+    <td>${title}</td>
+    <td>${annualSalary}</td>
     <td><button class="delete-btn">❌</button></td>
 </tr>
-`);
+`)
+}
+
+function deleteRow(event) {
+$(event.target).parent().parent().remove()
 }
